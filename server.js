48f6565b5a3211380266da6f6137f9164d7ef55f
Zahara Putri <email_github_kamu@example.com>
2026-05-18 19:51:29 +0700

    fix cors

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const db = require('./models/db') // 🔥 PENTING TAMBAH INI
 
 app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 
 // 🔥 DEBUG ENV
 console.log("DB_HOST =", process.env.DB_HOST)

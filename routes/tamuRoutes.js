4a979db6e97684a996456bd58f3ec6355e3cb169
Zahara Putri <email_github_kamu@example.com>
2026-06-04 22:37:02 +0700

    fix upload vercel

--- routes/tamuRoutes.js
+++ routes/tamuRoutes.js
@@ -1,17 +1,10 @@
 const express = require('express')
 const multer = require('multer')
 const router = express.Router()
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/')
-  },
 
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
+const upload = multer({
+  storage: multer.memoryStorage()
 })
-
-const upload = multer({ storage: storage })
 
 const tamuController = require('../controllers/tamuController')
 
@@ -23,7 +16,7 @@ router.post(
   upload.single('foto'),
   tamuController.createTamu
 )
-// FIX DISINI
+
 router.put('/edit/:id', tamuController.updateTamu)
 router.put('/keluar/:id', tamuController.keluarTamu)
 
